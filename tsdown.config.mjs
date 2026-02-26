00f4970a73a684db30ab3e6ee69e738fc13982b0
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-02-26 03:06:47 +0000

    vai

--- tsdown.config.mjs
+++ tsdown.config.mjs
@@ -4,6 +4,7 @@ export default defineConfig({
     entry: "./src/extension.ts",
     external: ["vscode"],
     inlineOnly: false,
+    sourcemap: true,
     minify: true,
     noExternal: ["colorjs.io"],
 });
