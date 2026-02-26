c994589b0da071861970d7044e5b8cfeba810bf2
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-02-26 02:56:25 +0000

    vai

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,31 +1,18 @@
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
-import prettier from "eslint-plugin-prettier/recommended";
-import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 export default [
-  {
-    files: ["**/*.ts", "**/*.mjs"],
-  },
-  {
-    plugins: {
-      "@typescript-eslint": typescriptEslint,
+    {
+        files: ["**/*.ts", "**/*.mjs"],
     },
-
-    languageOptions: {
-      parser: tsParser,
-      ecmaVersion: 2022,
-      sourceType: "module",
+    {
+        plugins: {
+            "@typescript-eslint": typescriptEslint,
+        },
+        languageOptions: {
+            parser: tsParser,
+            ecmaVersion: 2022,
+            sourceType: "module",
+        },
     },
-  },
-  {
-    plugins: {
-      "simple-import-sort": simpleImportSort,
-    },
-    rules: {
-      "simple-import-sort/imports": "error",
-      "simple-import-sort/exports": "error",
-    },
-  },
-  prettier,
 ];
--- tsdown.config.mjs
+++ tsdown.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
     entry: "./src/extension.ts",
-    outDir: "out",
+    external: ["vscode"],
+    inlineOnly: false,
     minify: true,
     noExternal: ["colorjs.io"],
-    external: ["vscode"],
 });
